72317f5d7553078aa82a41466be801a908902218
Benoit22324 <sunbenoit384@gmail.com>
2024-12-17 17:37:25 +0100

    Ajout des get/post

---
+++ data/moviesData.js
@@ -0,0 +1,22 @@
+export const movies = [
+    {
+        id: 1,
+        title: "Silent Voice",
+        genre: "Drama"
+    },
+    {
+        id: 2,
+        title: "Spy X Family Code: White",
+        genre: "Comédie"
+    },
+    {
+        id: 3,
+        title: "One Peace",
+        genre: "Action"
+    },
+    {
+        id: 4,
+        title: "I want to eat your pancreas",
+        genre: "Drama"
+    }
+]
