1a02c19cd38e57df55e5444d117f1066974dcfb8
Benoit22324 <sunbenoit384@gmail.com>
2024-12-18 10:46:17 +0100

    Ajout du router/update/delete

--- index.js
+++ index.js
@@ -1,44 +1,12 @@
 import express from "express";
 import "dotenv/config";
-import {movies} from "./data/moviesData.js";
+import moviesRouter from "./router/moviesRouter.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({extended : false}))
-
-app.get("/", (req, res) => {
-    res.send("Bienvenue sur le port 3002");
-});
-
-app.get("/movies", (req, res) => {
-    res.send(movies);
-});
-
-app.get("/movie/:id", (req, res) => {
-    const {id} = req.params;
-    try {
-        const movieById = movies.find(movie => movie.id === parseInt(id));
-
-        if(!movieById) return res.status(400).json({message: "Not Found"});
-
-        return res.json(movieById)
-    } catch (err) {
-        return res.status(500).json({message: "Error"})
-    }
-})
-
-app.post('/movies', (req, res) => {
-    let {title, genre} = req.body
-    console.log(req)
-    const newMovie = {
-        id : movies.length + 1,
-        title,
-        genre,
-    }
-    movies.push(newMovie)
-    return res.json(movies)
-})
+app.use(moviesRouter);
 
 app.listen(PORT, () => console.log(`Serveur allumé sur le port ${PORT}`));
---
+++ router/moviesRouter.js
@@ -0,0 +1,76 @@
+import { Router } from "express";
+import {movies} from "../data/moviesData.js"
+
+const moviesRouter = Router();
+
+moviesRouter.get("/", (req, res) => {
+    res.send("Bienvenue sur le port 3002");
+});
+
+moviesRouter.get("/movies", (req, res) => {
+    res.send(movies);
+});
+
+moviesRouter.get("/movie/:id", (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const movieById = movies.find(movie => movie.id === parseInt(id));
+
+        if(!movieById) return res.status(400).json({message: "Not Found"});
+
+        return res.json(movieById)
+    } catch(err) {
+        return res.status(500).json({message: `Error: ${err.message}`})
+    }
+})
+
+moviesRouter.post('/addMovie', (req, res) => {
+    let {title, genre} = req.body;
+
+    const newMovie = {
+        id : movies.length + 1,
+        title,
+        genre,
+    }
+
+    movies.push(newMovie)
+
+    return res.json(movies)
+})
+
+moviesRouter.put('/updateMovie/:id', (req, res) => {
+    let {id} = req.params;
+    let {title, genre} = req.body;
+
+    try {
+        if (!title || !genre) return res.status(400).json({message: "Need Title & Genre"});
+
+        const movieChoosen = movies.find(movie => movie.id === parseInt(id));
+        movieChoosen.title = title;
+        movieChoosen.genre = genre;
+
+        return res.json(movies);
+    } catch(err) {
+        return res.status(500).json({message: `Error: ${err.message}`})
+    }
+})
+
+moviesRouter.delete('/deleteMovie/:id', (req, res) => {
+    let {id} = req.params;
+
+    try {
+        const movieChoosen = movies.find(movie => movie.id == parseInt(id));
+
+        if (!movieChoosen) return res.status(400).json({message: "Not Found"})
+
+        const index = movies.indexOf(movieChoosen)
+        movies.splice(index, 1)
+
+        return res.json(movies);
+    } catch(err) {
+        return res.status(500).json({message: `Error: ${err.message}`})
+    }
+})
+
+export default moviesRouter
